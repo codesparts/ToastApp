1510b2c8fa890a8fa6c1ac6741b34e48fcdb16fb
coderuth <coderuth@outlook.com>
2016-11-05 21:13:41 +0530

    small

--- imageRec.py
+++ imageRec.py
@@ -10,9 +10,9 @@ class Image(object):
         app = ClarifaiApp()
         model = app.models.get('food-items-v1.0')
         image = app.inputs.create_image_from_bytes(img_bytes=self.url)
-        d1 = model.predict([image])
-        d2 = d1[u'outputs'][0][u'data'][u'concepts']
-        for i in d2:
+        predicted = model.predict([image])
+        items = predicted[u'outputs'][0][u'data'][u'concepts']
+        for i in items:
             item.append(i['name'])
         return item
 
